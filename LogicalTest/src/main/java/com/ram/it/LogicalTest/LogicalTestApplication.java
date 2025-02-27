f763317e04289ec3ad5e0751dbad5117ad590fc8
rameshjava18 <sheelamramesh3618@gmail.com>
2025-02-27 17:26:17 +0530

    First Commit

---
+++ LogicalTest/src/main/java/com/ram/it/LogicalTest/LogicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.ram.it.LogicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicalTestApplication.class, args);
+	}
+
+}
---
+++ LogicalTest/src/test/java/com/ram/it/LogicalTest/LogicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.it.LogicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
